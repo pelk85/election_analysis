1fe03d3ee101b2e13af948d235c8c27f24db845f
pelk85 <93356601+pelk85@users.noreply.github.com>
2022-01-08 14:58:20 -0500

    final upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -29,7 +29,7 @@ winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
 county_largest_turnout = ""
-county_count = 0
+county_largest_count = 0
 
 
 # Read the csv and convert it into a list of dictionaries
@@ -49,13 +49,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-        #Get the county name for each row 
         county_name = row[1]
-
-        #If the county is not already in the list, add it 
-        if county_name not in county_list:
-            #Add it to the list 
-            county_list.append(county_name)
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -72,17 +66,16 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
-
+        if county_name not in county_list:
+            
             # 4b: Add the existing county to the list of counties.
-
+            county_list.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            county_votes[county_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
-
+        county_votes[county_name] += 1
 
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
@@ -99,24 +92,35 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-
+    for county in county_votes:
         # 6b: Retrieve the county vote count.
-
+        county_total = county_votes.get(county)
         # 6c: Calculate the percentage of votes for the county.
-
+        county_percentage = float(county_total) / float(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
+        county_results = (
+            f"{county}: {county_percentage:.1f}% ({county_total:,})\n")
+        print(county_results)
 
          # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
+        if (county_total > county_largest_count):
+            county_largest_turnout = county
+            county_largest_count = county_total
 
 
     # 7: Print the county with the largest turnout to the terminal.
-
-
+    largest_county_print = ( 
+        f"\n-------------------------\n"
+        f"Largest County Turnout: {county_largest_turnout}"
+        "\n-------------------------\n"
+        )
+    print(largest_county_print)
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_county_print)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
--- README.md
+++ README.md
@@ -14,11 +14,17 @@ In order to complete a recount voter data for the Colorado Board of Elections we
     -Data Source: election_results.csv
     -Software: Python 3.6.7, Visual Studio Code 1.63.2
 
-## Summary 
+## Election Results 
 
 The analysis of the election shows that: 
 
     -There were 369,711 votes cast in this election
+
+    -Votes were cast in three different counties:
+        -The county of Jefferson contained 10.5% of the votes (38,855)
+        -The county of Arapahoe contained 6.7% of the votes (24,801)
+        -The county of Denver was the largest county with 82.8% of the votes (306,055)
+
     -The candidates were: 
         -Charles Casper Stockham
         -Diana DeGette
@@ -29,3 +35,13 @@ The analysis of the election shows that:
         -Raymon Anthony Doane recieved 3.1% of the votes (11.606)
     -The winner of the election was: 
         -Diana Degette, who recieved 272,892 votes, 73.8% of the total votes cast 
+
+## Summary 
+
+The process generated here has the capability to be adapted to future elections potentially streamlining the overall vote count process and decreasing the lag time it takes to report results. 
+
+If the Board of Elections would like to continue to utilize this algorythim in the future there are two key reccomendations to be considered: 
+
+First, the Board should look to use input variables for specific information about the election being reviewed. My adding some input variable statements to the script we could capture unique factors such as the region of the election, election title, year, etc. and not only write those to our output but customize the file name of the output based upon those inputs. 
+
+Secondly, the script could be modified to request input for the locations of the data within the csv file provided. This way you will not have to rely on ensuring the csv file is formatted in the same method as this original audit. If the script were to prompt for what columns in the csv hold the county and candidate name you could then place those varibles in the rest of the code to ensure it would be dynamic based on your unique csv. 
